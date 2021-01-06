e387c5918287e925004e350c0da81bfc1e6a6931
IhsenBen <72970267+IhsenBen@users.noreply.github.com>
2021-01-06 16:55:58 +0100

    -

---
+++ js/laureat_archive.js
@@ -0,0 +1,40 @@
+function archive1(){
+
+    document.getElementById("h3_cat1").innerHTML = "gagnant -2020-";
+    document.getElementById("h3_cat2").innerHTML = "gagnant -2020-";
+    document.getElementById("h3_cat3").innerHTML = "gagnant -2020-";
+    document.getElementById("h3_cat4").innerHTML = "gagnant -2020-";
+    document.getElementById("h3_cat5").innerHTML = "gagnant -2020-";
+}
+
+function archive2(){
+    document.getElementById("h3_cat1").innerHTML = "gagnant -2019-";
+    document.getElementById("h3_cat2").innerHTML = "gagnant -2019-";
+    document.getElementById("h3_cat3").innerHTML = "gagnant -2019-";
+    document.getElementById("h3_cat4").innerHTML = "gagnant -2019-";
+    document.getElementById("h3_cat5").innerHTML = "gagnant -2019-";
+    
+
+}
+function archive3(){
+    document.getElementById("h3_cat1").innerHTML = "gagnant -2018-";
+    document.getElementById("h3_cat2").innerHTML = "gagnant -2018-";
+    document.getElementById("h3_cat3").innerHTML = "gagnant -2018-";
+    document.getElementById("h3_cat4").innerHTML = "gagnant -2018-";
+    document.getElementById("h3_cat5").innerHTML = "gagnant -2018-";
+
+}
+function archive4(){
+    document.getElementById("h3_cat1").innerHTML = "gagnant -2017-";
+    document.getElementById("h3_cat2").innerHTML = "gagnant -2017-";
+    document.getElementById("h3_cat3").innerHTML = "gagnant -2017-";
+    document.getElementById("h3_cat4").innerHTML = "gagnant -2017-";
+    document.getElementById("h3_cat5").innerHTML = "gagnant -2017-";
+}
+function archve5(){
+    document.getElementById("h3_cat1").innerHTML = "gagnant -2017-";
+    document.getElementById("h3_cat2").innerHTML = "gagnant -2017-";
+    document.getElementById("h3_cat3").innerHTML = "gagnant -2017-";
+    document.getElementById("h3_cat4").innerHTML = "gagnant -2017-";
+    document.getElementById("h3_cat5").innerHTML = "gagnant -2017-";
+}
