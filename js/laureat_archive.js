928c5e3da1a1c82d88d41ecc64831c39568f4a61
IhsenBen <72970267+IhsenBen@users.noreply.github.com>
2021-01-06 21:46:25 +0100

    -

--- js/laureat_archive.js
+++ js/laureat_archive.js
@@ -1,40 +1,61 @@
-function archive1(){
+function archive1() {
 
     document.getElementById("h3_cat1").innerHTML = "gagnant -2020-";
     document.getElementById("h3_cat2").innerHTML = "gagnant -2020-";
     document.getElementById("h3_cat3").innerHTML = "gagnant -2020-";
     document.getElementById("h3_cat4").innerHTML = "gagnant -2020-";
-    document.getElementById("h3_cat5").innerHTML = "gagnant -2020-";
+
+
+    document.getElementById("img_cat1").src = '/image/faust.png';
+    document.getElementById("img_cat2").src = '/image/brother_karamazov.png';
+    document.getElementById("img_cat3").src = "/image/a dog's heart.png";
+    document.getElementById("img_cat4").src = '/image/huxley.png';
 }
 
-function archive2(){
+function archive2() {
     document.getElementById("h3_cat1").innerHTML = "gagnant -2019-";
     document.getElementById("h3_cat2").innerHTML = "gagnant -2019-";
     document.getElementById("h3_cat3").innerHTML = "gagnant -2019-";
     document.getElementById("h3_cat4").innerHTML = "gagnant -2019-";
     document.getElementById("h3_cat5").innerHTML = "gagnant -2019-";
-    
+
+
+    document.getElementById("img_cat1").src = '/image/.jpg';
+    document.getElementById("img_cat2").src = '/image/.jpg';
+    document.getElementById("img_cat3").src = '/image/celine.jpg';
+    document.getElementById("img_cat4").src = '/image/stendhal.jpg';
+
 
 }
-function archive3(){
+
+function archive3() {
     document.getElementById("h3_cat1").innerHTML = "gagnant -2018-";
     document.getElementById("h3_cat2").innerHTML = "gagnant -2018-";
     document.getElementById("h3_cat3").innerHTML = "gagnant -2018-";
     document.getElementById("h3_cat4").innerHTML = "gagnant -2018-";
     document.getElementById("h3_cat5").innerHTML = "gagnant -2018-";
 
+
+    document.getElementById("img_cat1").src = '/image/craft.jpg';
+    document.getElementById("img_cat2").src = '/image/buk.jpg';
+    document.getElementById("img_cat3").src = '/image/capital.jpg';
+    document.getElementById("img_cat4").src = '/image/stendhal.jpg';
+
 }
-function archive4(){
+
+
+function archive4() {
     document.getElementById("h3_cat1").innerHTML = "gagnant -2017-";
     document.getElementById("h3_cat2").innerHTML = "gagnant -2017-";
     document.getElementById("h3_cat3").innerHTML = "gagnant -2017-";
     document.getElementById("h3_cat4").innerHTML = "gagnant -2017-";
-    document.getElementById("h3_cat5").innerHTML = "gagnant -2017-";
+
+
+
+
+    document.getElementById("img_cat1").src = '/image/craft.jpg';
+    document.getElementById("img_cat2").src = '/image/buk.jpg';
+    document.getElementById("img_cat3").src = '/image/celine.jpg';
+    document.getElementById("img_cat4").src = '/image/stendhal.jpg';
 }
-function archve5(){
-    document.getElementById("h3_cat1").innerHTML = "gagnant -2017-";
-    document.getElementById("h3_cat2").innerHTML = "gagnant -2017-";
-    document.getElementById("h3_cat3").innerHTML = "gagnant -2017-";
-    document.getElementById("h3_cat4").innerHTML = "gagnant -2017-";
-    document.getElementById("h3_cat5").innerHTML = "gagnant -2017-";
-}
+
